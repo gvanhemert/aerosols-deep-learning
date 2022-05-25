8f5f31e539e6d8262f9eb980a013e24a51b6e610
gvanhemert <91330305+gvanhemert@users.noreply.github.com>
2022-05-25 11:25:48 +0200

    Add files via upload

--- tfrecord-write-emissions.py
+++ tfrecord-write-emissions.py
@@ -15,18 +15,28 @@ from sklearn.model_selection import train_test_split
 path = 'C:/Users/Guus van Hemert/Desktop/TW/TW5/Thesis/Data/Yearly Data/'
 ensemble_path = Path('C:/Users/Guus van Hemert/Desktop/TW/TW5/Thesis/Data/Ensembles/')
 emission_path = 'C:/Users/Guus van Hemert/Desktop/TW/TW5/Thesis/Data/Emissions/'
+emission_path_2 = Path('C:/Users/Guus van Hemert/Desktop/TW/TW5/Thesis/Data/Emissions/')
 
 ensembles = {}
-for a in ensemble_path.glob("*"):
-    if str(a.parts[-1][-2:]) == 'nc' and int(a.parts[-1][:-3]) >= 17:
-        ensembles[int(a.parts[-1][:-3])] = xr.open_dataset(a)
+ensembles['nat'] = {}
+ensembles['ctl'] = {}
+for a in emission_path_2.glob("*"):
+    try:
+        if str(a.parts[-1][-2:]) == 'nc' and int(a.parts[-1][1:3]) <= 8 and str(a.parts[-1][0]) == 'C':
+            ensembles['ctl'][a.parts[-1][:-3]] = xr.open_dataset(a)
+        if str(a.parts[-1][-2:]) == 'nc' and int(a.parts[-1][1:3]) <= 8 and str(a.parts[-1][0]) == 'N':
+            ensembles['nat'][a.parts[-1][:-3]] = xr.open_dataset(a)
+    except:
+        print('Yikes')
+        #if str(a.parts[-1][-2:]) == 'nc':
+            #ensembles[a.parts[-1][:-3]] = xr.open_dataset(a)
     
 
 # Reading datasets
-ctl = xr.open_dataset(path + 'CTL.nc')
+ctl = xr.open_dataset(path + 'C01.nc')
 nat = xr.open_dataset(path + 'NAT.nc')
 
-ctl_emi = xr.open_dataset(emission_path + 'CTL_emi.nc')
+ctl_emi = xr.open_dataset(emission_path + 'C01_emi.nc')
 nat_emi = xr.open_dataset(emission_path + 'NAT_emi.nc')
 
 spex_one = xr.open_dataset(path + 'SPEXone_Mask.nc')
@@ -58,7 +68,7 @@ def create_spex3_input_images(df, input_label):
      returns an array containing the reshaped arrays from the chosen column containing arrays'''
     input_image = []
     for i in df.index:
-        input_image.append(df[input_label][i].reshape(96, 192, 3))
+        input_image.append(df[input_label][i].reshape(96, 192, 1))
     return np.array(input_image)
 
 def _bytes_feature(value):
@@ -93,14 +103,16 @@ def parse_combined_data(ctl, ctl_lat, ctl_lon, spex, nat):
         'depth': _int64_feature(spex['aod_550'].shape[2]),
     }
     
-    for key in ctl.keys():
+    for key in nat.keys():
+        data['nat_'+key] = _bytes_feature(serialize_array(nat[key]))
         data['ctl_'+key] = _bytes_feature(serialize_array(ctl[key]))
-        data['ctl_lat_'+key] = _bytes_feature(serialize_array(ctl_lat[key]))
-        data['ctl_lon_'+key] = _bytes_feature(serialize_array(ctl_lon[key]))
         if key in spex.keys():
             data['spex_'+key] = _bytes_feature(serialize_array(spex[key]))
-        if key in nat.keys():
-            data['nat_'+key] = _bytes_feature(serialize_array(nat[key]))
+            data['ctl_lat_'+key] = _bytes_feature(serialize_array(ctl_lat[key]))
+            data['ctl_lon_'+key] = _bytes_feature(serialize_array(ctl_lon[key]))
+        
+        #if key in nat.keys():
+            #data['nat_'+key] = _bytes_feature(serialize_array(nat[key]))
 
     out = tf.train.Example(features=tf.train.Features(feature=data))
 
@@ -142,10 +154,11 @@ def write_data(ctl, ctl_lat, ctl_lon, spex, nat,
                 current_ctl_lat[key] = ctl_lat[key][index]
                 current_ctl_lon[key] = ctl_lon[key][index]
                 current_spex[key] = spex[key][index]
+                #current_nat[key] = nat[key][index]
             for key in nat.keys():
                 current_ctl[key] = ctl[key][index]
-                current_ctl_lat[key] = ctl_lat[key][index]
-                current_ctl_lon[key] = ctl_lon[key][index]
+                #current_ctl_lat[key] = ctl_lat[key][index]
+                #current_ctl_lon[key] = ctl_lon[key][index]
                 current_nat[key] = nat[key][index]
 
             out = parse_combined_data(ctl=current_ctl,                                    
@@ -228,103 +241,143 @@ for key in parameters_1:
     NAT[key] = {}
     spex_3_day[key] = {}
 
-for key in parameters_1:
-    for n in range(n_days):
-        if key == 'aod_550':
-            CTL[key][n] = ctl.TAU_2D_550nm.data[n]
-            NAT[key][n] = nat.TAU_2D_550nm.data[n]
-            
-        if key == 'ssa_550':
-            CTL[key][n] = ctl.OMEGA_2D_550nm.data[n]
-            NAT[key][n] = nat.OMEGA_2D_550nm.data[n]
-            
-        if key == 'ae':
-            CTL[key][n] = ctl.ANG_550nm_865nm.data[n]
-            NAT[key][n] = nat.ANG_550nm_865nm.data[n]
-            
-        NAT[key][n] = (NAT[key][n] - np.min(NAT[key][n])) / (np.max(NAT[key][n]) - np.min(NAT[key][n]))
-            
-        CTL_lat[key][n] = np.gradient(CTL[key][n], axis=0)
-        CTL_lon[key][n] = np.gradient(CTL[key][n], axis=1)
-        spex[key][n] = spex_one.Count.data[n]
-        spex[key][n] = np.where(np.isnan(spex[key][n]), spex[key][n],
-                                   NAT[key][n] * spex[key][n])
-        spex[key][n] = np.nan_to_num(spex[key][n], nan=-1).reshape(96, 192, 1)
-    for n in range(1, n_days-1):
-        spex_3_day[key][n] = np.concatenate((spex[key][n-1], spex[key][n],
-                                        spex[key][n+1]), axis=-1)
-    del CTL[key][0], CTL[key][n_days-1]
-    del CTL_lat[key][0], CTL_lat[key][n_days-1]
-    del CTL_lon[key][0], CTL_lon[key][n_days-1]
-    del NAT[key][0], NAT[key][n_days-1]
+for ens_key in range(1):
+    #ctl = ensembles['ctl']['C0'+str(ens_key+1)]
+    #nat = ensembles['nat']['N0'+str(ens_key+1)]
+    #ctl_emi = ensembles['ctl']['C0'+str(ens_key+1)+'_emi']
+    #nat_emi = ensembles['nat']['N0'+str(ens_key+1)+'_emi']
     
-for name, var in nat_emi.items():
-    NAT[name] = {}
-    if name[0] == 'e':
-        parameters_2.append(name)
+    CTL = {}
+    CTL_lat = {}
+    CTL_lon = {}
+    spex = {}
+    NAT = {}
+    spex_3_day = {}
+    
+    parameters_1 = ['aod_550', 'ssa_550', 'ae']
+    parameters_2 = []
+
+    for key in parameters_1:
+        CTL[key] = {}
+        CTL_lat[key] = {}
+        CTL_lon[key] = {}
+        spex[key] = {}
+        NAT[key] = {}
+        spex_3_day[key] = {}
+    for key in parameters_1:
         for n in range(n_days):
-            NAT[name][n] = var.data[n]
-        del NAT[name][0], NAT[name][n_days-1]
-
-for name, var in ctl_emi.items():
-    CTL[name] = {}
-    CTL_lat[name] = {}
-    CTL_lon[name] = {}
-    if name[0] == 'e':
-        for n in range(n_days):
-            CTL[name][n] = var.data[n]
-            CTL_lat[name][n] = np.gradient(CTL[name][n], axis=0)
-            CTL_lon[name][n] = np.gradient(CTL[name][n], axis=1)
-        del CTL[name][0], CTL[name][n_days-1]
-        del CTL_lat[name][0], CTL_lat[name][n_days-1]
-        del CTL_lon[name][0], CTL_lon[name][n_days-1]
-
-
-
-CTL_list, CTL_lat_list, CTL_lon_list, spex_list, NAT_list  = [], [], [], [], []
-for key in parameters_1:
-    CTL_list.append(pd.DataFrame(CTL[key].items(), columns=['Day', key]).drop('Day', axis=1))
-    CTL_lat_list.append(pd.DataFrame(CTL_lat[key].items(), columns=['Day', key]).drop('Day', axis=1))
-    CTL_lon_list.append(pd.DataFrame(CTL_lon[key].items(), columns=['Day', key]).drop('Day', axis=1))
-    spex_list.append(pd.DataFrame(spex_3_day[key].items(), columns=['Day', key]).drop('Day', axis=1))
+            if key == 'aod_550':
+                CTL[key][n] = ctl.TAU_2D_550nm.data[n]
+                NAT[key][n] = nat.TAU_2D_550nm.data[n]
+                
+            if key == 'ssa_550':
+                CTL[key][n] = ctl.OMEGA_2D_550nm.data[n]
+                NAT[key][n] = nat.OMEGA_2D_550nm.data[n]
+                
+            if key == 'ae':
+                CTL[key][n] = ctl.ANG_550nm_865nm.data[n]
+                NAT[key][n] = nat.ANG_550nm_865nm.data[n]
+            
+            NAT_norm = (NAT[key][n] - np.min(NAT[key][n])) / (np.max(NAT[key][n]) - np.min(NAT[key][n]))
+                
+            CTL_lat[key][n] = np.gradient(CTL[key][n], axis=0)
+            CTL_lon[key][n] = np.gradient(CTL[key][n], axis=1)
+            spex[key][n] = spex_one.Count.data[n]
+            spex[key][n] = np.where(np.isnan(spex[key][n]), spex[key][n],
+                                      NAT[key][n] * spex[key][n])
+            #spex[key][n] = np.nan_to_num(spex[key][n], nan=-1).reshape(96, 192, 1)
+        for n in range(n_days-6):
+            #spex_3_day[key][n] = np.concatenate([spex[key][n + i] for i in range(7)], axis=-1)
+            spex_3_day[key][n] = np.nanmean(np.array([spex[key][n + 1] for i in range(7)]), axis=0)
+            #spex_3_day[key][n] = np.nan_to_num(spex[key][n], nan=-1).reshape(96, 192, 1)
+            CTL[key][n] = np.mean(np.array([CTL[key][n + i] for i in range(7)]), axis=0)
+            NAT[key][n] = np.mean(np.array([NAT[key][n + i] for i in range(7)]), axis=0)
+        for j in range(6):
+            del CTL[key][n_days - j - 1]
+            del CTL_lat[key][n_days - j - 1]
+            del CTL_lon[key][n_days - j - 1]
+            del NAT[key][n_days - j - 1]
     
-for key in parameters_2:
-    CTL_list.append(pd.DataFrame(CTL[key].items(), columns=['Day', key]).drop('Day', axis=1))
-    CTL_lat_list.append(pd.DataFrame(CTL_lat[key].items(), columns=['Day', key]).drop('Day', axis=1))
-    CTL_lon_list.append(pd.DataFrame(CTL_lon[key].items(), columns=['Day', key]).drop('Day', axis=1))
-    NAT_list.append(pd.DataFrame(NAT[key].items(), columns=['Day', key]).drop('Day', axis=1))
+    for name, var in nat_emi.items():
+        NAT[name] = {}
+        if name[0] == 'e':
+            parameters_2.append(name)
+            for n in range(n_days):
+                NAT[name][n] = var.data[n]
+            for n in range(n_days-6):
+                NAT[name][n] = np.mean(np.array([NAT[name][n + i] for i in range(7)]), axis=0)
+            for j in range(6):
+                del NAT[name][n_days - j - 1]
     
-CTL_df = pd.concat(CTL_list, axis=1)
-CTL_lat_df = pd.concat(CTL_lat_list, axis=1)
-CTL_lon_df = pd.concat(CTL_lon_list, axis=1)
-spex_df = pd.concat(spex_list, axis=1)
-NAT_df = pd.concat(NAT_list, axis=1)
-
-CTL = {}
-CTL_lat = {}
-CTL_lon = {}
-spex = {}
-NAT = {}
-spex_3_day = {}
-
-#Input and output data
-for key in parameters_1:
-    CTL[key] = create_input_images(CTL_df, key)
-    CTL_lat[key] = create_input_images(CTL_lat_df, key)
-    CTL_lon[key] = create_input_images(CTL_lon_df, key)
-    spex[key] = create_spex3_input_images(spex_df, key)
-    spex[key] = spex[key].astype("float32")
+    for name, var in ctl_emi.items():
+        CTL[name] = {}
+        #CTL_lat[name] = {}
+        #CTL_lon[name] = {}
+        if name[0] == 'e':
+            for n in range(n_days):
+                CTL[name][n] = var.data[n]
+                #CTL_lat[name][n] = np.gradient(CTL[name][n], axis=0)
+                #CTL_lon[name][n] = np.gradient(CTL[name][n], axis=1)
+            for n in range(n_days-6):
+                CTL[name][n] = np.mean(np.array([CTL[name][n + i] for i in range(7)]), axis=0)
+                #CTL_lat[name][n] = np.gradient(CTL[name][n], axis=0)
+                #CTL_lon[name][n] = np.gradient(CTL[name][n], axis=1)
+                
+            for j in range(6):
+                del CTL[name][n_days-j-1]
+                #del CTL_lat[name][n_days-j-1]
+                #del CTL_lon[name][n_days-j-1]
     
-for key in parameters_2:
-    CTL[key] = create_input_images(CTL_df, key)
-    CTL_lat[key] = create_input_images(CTL_lat_df, key)
-    CTL_lon[key] = create_input_images(CTL_lon_df, key)
-    NAT[key] = create_input_images(NAT_df, key)
     
+    
+    CTL_list, CTL_lat_list, CTL_lon_list, spex_list, NAT_list  = [], [], [], [], []
+    for key in parameters_1:
+        CTL_list.append(pd.DataFrame(CTL[key].items(), columns=['Day', key]).drop('Day', axis=1))
+        CTL_lat_list.append(pd.DataFrame(CTL_lat[key].items(), columns=['Day', key]).drop('Day', axis=1))
+        CTL_lon_list.append(pd.DataFrame(CTL_lon[key].items(), columns=['Day', key]).drop('Day', axis=1))
+        spex_list.append(pd.DataFrame(spex_3_day[key].items(), columns=['Day', key]).drop('Day', axis=1))
+        NAT_list.append(pd.DataFrame(NAT[key].items(), columns=['Day', key]).drop('Day', axis=1))
+        
+    for key in parameters_2:
+        CTL_list.append(pd.DataFrame(CTL[key].items(), columns=['Day', key]).drop('Day', axis=1))
+        #CTL_lat_list.append(pd.DataFrame(CTL_lat[key].items(), columns=['Day', key]).drop('Day', axis=1))
+        #CTL_lon_list.append(pd.DataFrame(CTL_lon[key].items(), columns=['Day', key]).drop('Day', axis=1))
+        NAT_list.append(pd.DataFrame(NAT[key].items(), columns=['Day', key]).drop('Day', axis=1))
+        
+    CTL_df = pd.concat(CTL_list, axis=1)
+    CTL_lat_df = pd.concat(CTL_lat_list, axis=1)
+    CTL_lon_df = pd.concat(CTL_lon_list, axis=1)
+    spex_df = pd.concat(spex_list, axis=1)
+    NAT_df = pd.concat(NAT_list, axis=1)
+    
+    CTL = {}
+    CTL_lat = {}
+    CTL_lon = {}
+    spex = {}
+    NAT = {}
+    spex_3_day = {}
+    
+    #Input and output data
+    for key in parameters_1:
+        CTL[key] = create_input_images(CTL_df, key)
+        CTL_lat[key] = create_input_images(CTL_lat_df, key)
+        CTL_lon[key] = create_input_images(CTL_lon_df, key)
+        spex[key] = create_spex3_input_images(spex_df, key)
+        spex[key] = spex[key].astype("float32")
+        NAT[key] = create_input_images(NAT_df, key)
+        
+    for key in parameters_2:
+        CTL[key] = create_input_images(CTL_df, key)
+        #CTL_lat[key] = create_input_images(CTL_lat_df, key)
+        #CTL_lon[key] = create_input_images(CTL_lon_df, key)
+        NAT[key] = create_input_images(NAT_df, key)
+    
+    write_data(CTL, CTL_lat, CTL_lon, spex, NAT, max_files=100,
+               filename=str(ens_key+1), out_dir=emission_path+"Network/test_mean_emissions_7days_C01/")
 
 #%% Write to tfrecord
 write_data(CTL, CTL_lat, CTL_lon, spex, NAT, max_files=100,
-           filename='test_emissions_7days', out_dir=emission_path+"Network/test_emissions_7days/")
+           filename='test_mean_emissions_7days', out_dir=emission_path+"Network/test_mean_emissions_7days/")
 
 
 #%% Testing
